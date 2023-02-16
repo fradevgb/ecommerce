163f7215e22e150f191a0e95c08f53fb4fcba6ad
Frank Gaspar <frank.gasparfcytsis@gmail.com>
2023-02-16 18:01:21 -0400

    Render products in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,25 @@
+import { GetServerSidePropsContext } from 'next';
 import Head from 'next/head';
-import { Heading } from '@chakra-ui/react';
 
-export default function Home() {
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    count: number;
+    rate: number;
+  };
+};
+
+type Props = {
+  products: Product[];
+};
+
+export default function Home({ products }: Props) {
+  console.log(products);
   return (
     <>
       <Head>
@@ -11,8 +29,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Heading>We got chackra</Heading>
+        <ol>
+          {products.map((product) => {
+            return <li key={product.id}>{product.title}</li>;
+          })}
+        </ol>
       </main>
     </>
   );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const products = await fetch('https://fakestoreapi.com/products').then((res) => res.json());
+
+  return {
+    props: {
+      products,
+    },
+  };
 }
