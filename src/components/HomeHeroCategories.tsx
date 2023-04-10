3d3a0df98ddf5a2a3354f751d0559fc1cfbecde3
Frank Gaspar <frank.gasparfcytsis@gmail.com>
2023-04-10 16:01:36 -0400

    Add responsive for hero categories component

--- src/components/HomeHeroCategories.tsx
+++ src/components/HomeHeroCategories.tsx
@@ -1,6 +1,6 @@
 import { Categories } from "@/models/Categories";
 import { slugify } from "@/Utils/slugify";
-import { Grid, GridItemProps, GridItem } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 import Image from "next/image";
 import { CenteredLabel } from "./CenteredLabel";
 
@@ -9,25 +9,14 @@ type Props = {
 };
 
 export function HomeHeroCategories({ categories }: Props) {
-  return (<Grid templateColumns="540px 255px 255px" templateRows="200px 260px" gap="30px">
-    {categories.map((cat, key) => {
-      const slug = slugify(cat);
-      const imageUrl = `/pic-categories-${slug}.jpg`;
-      let gridItemProps: GridItemProps = {
-        position: "relative",
-        w: "100%",
-        h: "100%"
-      };
+  return (
+    <Grid templateColumns={{ base: "1fr 1fr", md: "540px 255px 255px" }} templateRows={{ base: "130px 154px 130px", md: "200px 260px" }} gap={{ base: "0.5rem", md: "30px" }} templateAreas={{ base: `"cat1 cat1" "cat2 cat3" "cat4 cat4"`, md: `"cat1 cat2 cat3" "cat1 cat4 cat4"` }}>
+      {categories.map((cat, index) => {
+        const slug = slugify(cat);
+        const imageUrl = `/pic-categories-${slug}.jpg`;
 
-      if (key === 0) {
-        gridItemProps.rowSpan = 2;
-      }
-
-      if (key === categories.length - 1) {
-        gridItemProps.colSpan = 2;
-      }
-
-      return <GridItem {...gridItemProps} key={key}><Image src={imageUrl} fill={true} alt={cat} /><CenteredLabel>{cat}</CenteredLabel></GridItem>;
-    })}
-  </Grid>);
+        // TODO: Fix the image size crop issue
+        return <GridItem fontSize={{ base: "0.85rem", md: "1rem" }} position="relative" w="100%" height="100%" gridArea={`cat${index + 1}`} key={index}><Image src={imageUrl} style={{ objectFit: "cover" }} fill={true} alt={cat} /><CenteredLabel>{cat}</CenteredLabel></GridItem>;
+      })}
+    </Grid >);
 }
