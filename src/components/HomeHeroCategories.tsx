b17b3eeabfe9ed52a97a281e34bc7a9d4b12bdf8
Frank Gaspar <frank.gasparfcytsis@gmail.com>
2023-06-05 15:44:28 -0400

    Add iPad categories hero support

--- src/components/HomeHeroCategories.tsx
+++ src/components/HomeHeroCategories.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 export function HomeHeroCategories({ categories }: Props) {
   return (
-    <Grid templateColumns={{ base: "1fr 1fr", md: "540px 255px 255px" }} templateRows={{ base: "130px 154px 130px", md: "200px 260px" }} gap={{ base: "0.5rem", md: "30px" }} templateAreas={{ base: `"cat1 cat1" "cat2 cat3" "cat4 cat4"`, md: `"cat1 cat2 cat3" "cat1 cat4 cat4"` }}>
+    <Grid templateColumns={{ base: "1fr 1fr", md: "2fr 1fr 1fr" }} templateRows={{ base: "130px 154px 130px", md: "200px 260px" }} gap={{ base: "0.5rem", md: "1rem", lg: "2rem" }} templateAreas={{ base: `"cat1 cat1" "cat2 cat3" "cat4 cat4"`, md: `"cat1 cat2 cat3" "cat1 cat4 cat4"` }}>
       {categories.map((cat, index) => {
         const slug = slugify(cat);
         const imageUrl = `/pic-categories-${slug}.jpg`;
